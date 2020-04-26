62e2191ecc6069ef67c0881512ae765ea579b97b
Fakir117 <diogommsilva97@gmail.com>
2020-04-26 23:18:13 +0100

    TP9

---
+++ TP9/interface/src/components/ConsultaAtor.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-alert type="warning" v-if="!atorCarregado">
+      A carregar informação...
+    </v-alert>
+    
+    <v-card v-else>
+        <v-card-title class="indigo darken-4 white--text" dark>
+            <span class="headline">Ator: "{{ ator.info.anome }}" ({{idAtor}})</span>
+        </v-card-title>
+
+        <v-card-text>
+
+          <Filmes :lista="ator.filmes" />
+          <Personagens :lista="ator.personagens" />
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" @click="$router.go(-1)">Voltar</v-btn>
+        </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const lhost = require("@/config/global").host;
+
+import Filmes from '@/components/Filmes.vue'
+import Personagens from '@/components/Personagens.vue'
+
+export default {
+  name: 'ConsultaAtor',
+
+  components: {
+    Personagens, Filmes
+  },
+
+  props: ["idAtor"],
+
+  data: () => ({
+    ator: {},
+    atorCarregado: false,
+
+    footer_props: {
+      "items-per-page-text": "Mostrar",
+      "items-per-page-options": [10, 20, 50, 100, -1],
+      "items-per-page-all-text": "Todos"
+    }
+  }),
+
+  created: async function(){
+    try {
+      let response = await axios.get(lhost + "/atores/" + this.idAtor);
+      this.ator = response.data;
+      this.atorCarregado = true;
+    } 
+    catch (e) {
+      return e;
+    }
+  },
+
+  methods: {}
+  
+}
+</script>
+
+<style>
+    .info-label {
+      color: indigo;
+      padding: 5px;
+      font-weight: 400;
+      width: 100%;
+      background-color: #e0f2f1;
+      font-weight: bold;
+    }
+
+    .info-content {
+      padding: 5px;
+      width: 100%;
+      border: 1px solid #1a237e;
+    }
+</style>
+
